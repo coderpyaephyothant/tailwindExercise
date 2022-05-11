1b7e0c88d9676242185b1c7a62c8911748197e21
pyae phyo thant <kothant.kt@gmail.com>
2022-05-11 10:20:28 +0630

    Tailwind css first experience

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px'
+    },
+    extend: {
+      colors: {
+        brightRed:'hsl(12,88%,59%)',
+        DarkGrayishBlue: 'hsl(227, 12%, 61%)',
+        VeryDarkBlue: 'hsl(233, 12%, 13%)',
+        VeryPaleRed: 'hsl(13, 100%, 96%)',
+        VaryLightGray: 'hsl(0, 0%, 98%)'
+      }
+    },
+  },
+  plugins: [],
+}
